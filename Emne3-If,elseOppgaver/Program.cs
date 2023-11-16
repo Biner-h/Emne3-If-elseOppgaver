03f71844312214fc08c5ed88f48669f505cf5104
Biner Habib <Binerhabib@gmail.com>
2023-11-16 10:39:44 +0100

    Add project files.

---
+++ Emne3-If,elseOppgaver/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Emne3_If_elseOppgaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

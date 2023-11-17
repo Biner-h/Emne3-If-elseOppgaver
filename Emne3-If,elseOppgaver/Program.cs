e3c16d1e1c601a2c3ae67501a3583bab2e124690
Biner Habib <Binerhabib@gmail.com>
2023-11-17 12:28:02 +0100

    DONE

--- Emne3-If,elseOppgaver/Program.cs
+++ Emne3-If,elseOppgaver/Program.cs
@@ -1,28 +1,31 @@
-﻿namespace Emne3_If_elseOppgaver
-{
-    internal class Program
+﻿
+    getRanNum();
+
+    void getRanNum()
     {
-        public static void getRanNum()
+        Random random = new Random();
+        bool isEqual = false;
+        int counter = 0;
+
+        while (true)
         {
-            Random random = new Random();
             int number1 = random.Next(1, 5 + 1);
             int number2 = random.Next(1, 6);
-            bool isEqual = false;
+
 
             if (number1 == number2)
             {
                 isEqual = true;
-                Console.WriteLine("Det er riktig");
+                Console.WriteLine($"Det er riktig. Det tok deg {counter} forsøk!");
+                break;
             }
             else
             {
                 isEqual = false;
                 Console.WriteLine("Tallene er ikke like!");
-            }           
-        }
-        static void Main()
-        {
-            getRanNum();
+                counter++;
+            }
         }
     }
-}
+
+        

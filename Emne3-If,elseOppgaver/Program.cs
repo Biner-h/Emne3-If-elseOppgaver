664a7e4724a2b5c62765581996ef6b37e8082746
Biner Habib <Binerhabib@gmail.com>
2023-11-17 13:30:34 +0100

    DONE!!!!

--- Emne3-If,elseOppgaver/Program.cs
+++ Emne3-If,elseOppgaver/Program.cs
@@ -1,8 +1,11 @@
 ﻿
-    getRanNum();
+    oppgave1();
+    oppgave2();
+    oppgave3();
 
-    void getRanNum()
+    void oppgave1()
     {
+        Console.WriteLine("Oppgave 1!\n");
         Random random = new Random();
         bool isEqual = false;
         int counter = 0;
@@ -16,7 +19,7 @@
             if (number1 == number2)
             {
                 isEqual = true;
-                Console.WriteLine($"Det er riktig. Det tok deg {counter} forsøk!");
+                Console.WriteLine($"Det er riktig. Det tok deg {counter} forsøk!\n");
                 break;
             }
             else
@@ -25,6 +28,48 @@
                 Console.WriteLine("Tallene er ikke like!");
                 counter++;
             }
+        }
+    }
+
+    void oppgave2()
+    {
+        Console.WriteLine("Oppgave 2!\n");
+        Random rand = new Random();
+        bool equal = true;        
+        
+        int number3 = 4;
+        int number4 = 4;
+
+        if (number3 == number4)
+        {
+            equal = true;
+            Console.WriteLine(number3 * number4);
+            Console.WriteLine("");
+    }
+        else
+        {
+            equal = false;
+            Console.WriteLine(number3 + number4);
+            Console.WriteLine("");
+    }
+    }
+
+    void oppgave3()
+    {
+        Console.WriteLine("Oppgave 3\n");
+        Random oppgave3 = new Random();
+        int number5 = 10;
+        int number6 = 20;
+        int number7 = 30;
+        bool erSann = true;
+        
+        if (number5 + number6 == number7) 
+        {
+            Console.WriteLine(erSann);
+        }
+        else
+        {
+            Console.WriteLine(!erSann);
         }
     }
 
